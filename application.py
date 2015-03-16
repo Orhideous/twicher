67fad1bb987160065903df0599da3695c57fb7b2
Andriy Kushnir (Orhideous) <orhideous@gmail.com>
2015-03-16 23:05:53 +0200

    API scaffold

--- application.py
+++ application.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
+from flask.ext.restful import Api
 from flask.ext.assets import Environment
+
 from assets import bundles
+from resources import Quote, QuotesList, Random
 
 app = Flask(__name__)
 assets = Environment(app)
+api = Api(app)
+
 assets.register(bundles)
+
+api.add_resource(QuotesList, '/quotes')
+api.add_resource(Quote, '/quotes/<quote_id>')
+api.add_resource(Random, '/quotes/twitchy')
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template("main.html")
---
+++ resources.py
@@ -0,0 +1,28 @@
+from flask.ext.restful import Resource
+
+
+class QuotesList(Resource):
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class Quote(Resource):
+
+    def get(self):
+        pass
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
+
+
+class Random(Resource):
+
+    def get(self):
+        pass
