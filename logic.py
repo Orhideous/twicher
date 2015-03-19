7ae170a78ec44bce4d61c20010cd1b7a350642bb
Andriy Kushnir (Orhideous) <orhideous@gmail.com>
2015-03-19 02:42:03 +0200

    BL skeleton

---
+++ logic.py
@@ -0,0 +1,26 @@
+from pony.orm import db_session
+from models import Quote
+
+
+def get_all():
+    return []
+
+
+def get_random():
+    return
+
+
+def create(text):
+    return
+
+
+def read(quote_id):
+    return
+
+
+def update(quote_id, text):
+    return
+
+
+def delete(quote_id):
+    return
--- resources.py
+++ resources.py
@@ -1,28 +1,40 @@
-from flask.ext.restful import Resource
+from flask.ext.restful import Resource, marshal_with
+from flask.ext.restful.reqparse import RequestParser
+
+import logic
+from utils import quote_marshaller
+
+parser = RequestParser()
+parser.add_argument('text', type=str, required=True)
 
 
 class QuotesList(Resource):
 
+    @marshal_with(quote_marshaller)
     def get(self):
-        pass
+        return logic.get_all()
 
     def post(self):
-        pass
+        text = parser.parse_args()
+        return logic.create(text)
 
 
 class Quote(Resource):
 
+    @marshal_with(quote_marshaller)
     def get(self, quote_id):
-        pass
+        return logic.read(quote_id)
 
     def put(self, quote_id):
-        pass
+        text = parser.parse_args()
+        return logic.update(quote_id, text)
 
     def delete(self, quote_id):
-        pass
+        return logic.delete(quote_id)
 
 
 class Random(Resource):
 
+    @marshal_with(quote_marshaller)
     def get(self):
-        pass
+        return logic.get_random()
