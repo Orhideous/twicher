b49aefcb450f498c666c0c342032ba7b950dcb1e
Andriy Kushnir (Orhideous) <orhideous@gmail.com>
2016-06-21 03:27:00 +0300

    Replace gulp+browserify with webpack

--- twicher/frontend/js/main.coffee
+++
@@ -1,6 +0,0 @@
-{render} = require 'react'
-{List} = require 'components.coffee'
-
-node = document.getElementById 'excerpt_list'
-
-render List(), node
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+var ExtractTextPlugin, aliases, isProduction, keys, path, vendors, webpack;
+
+path = require('path');
+
+webpack = require('webpack');
+
+keys = require('lodash').keys;
+
+ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+isProduction = process.env.NODE_ENV === 'production';
+
+vendors = ["lodash", "react", "rx"];
+
+var stylusSettings = {
+    paths: "node_modules/bootstrap-styl",
+    "resolve url": 1
+};
+
+module.exports = {
+    context: path.join(__dirname, 'frontend'),
+    cache: true,
+    entry: {
+        main: "./main",
+        vendor: vendors
+    },
+    output: {
+        path: path.join(__dirname, 'twicher', 'static'),
+        publicPath: "./",
+        filename: '[name].bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.styl$/,
+                loader: ExtractTextPlugin.extract(
+                    "style-loader",
+                    "css!stylus?" + JSON.stringify(stylusSettings)
+                )
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url",
+                query: {
+                    limit: 10000,
+                    mimetype: 'application/font-woff'
+                }
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file"
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.styl',],
+        modulesDirectories: ['node_modules', 'scripts']
+    },
+    plugins: Array.prototype.concat(isProduction ? [new webpack.optimize.UglifyJsPlugin()] : [], [
+        new ExtractTextPlugin("styles.css"), new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: Infinity
+        })
+    ])
+};
